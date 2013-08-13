9e05671c1b585ef3faf099c474d53402c6a361d1
Contra <contra@maricopa.edu>
2013-08-13 00:18:13 -0700

    rickroll everything

--- examples/rickroll-spam.js
+++ examples/rickroll-spam.js
@@ -3,16 +3,15 @@ var nodecast = require('../');
 var stream = nodecast.find();
 
 stream.on('device', function(device) {
-	// Filter for our chromecast device
-	// This library works fine with other DIAL devices but this is a demo
-	if (device.info.modelName !== "Eureka Dongle") return;
+	// This is how you would do a device filter
+	//if (device.info.modelName !== "Eureka Dongle") return;
 
-	console.log('Found ChromeCast', device.name);
+	console.log('Found device', device.name);
 
 	var youtube = device.app('YouTube');
 
 	youtube.start('v=oHg5SJYRHA0', function(err) {
 		if (err) console.log('error starting', err);
-		console.log('Started');
+		console.log('Started on', device.name);
 	});
 });
