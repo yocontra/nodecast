b729032078b34a1b4160894962dc420c1ca74177
Contra <contra@maricopa.edu>
2013-08-13 00:54:34 -0700

    bin files

--- examples/spam.js
+++ examples/spam.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 
 var nodecast = require('../');
+var argv = require('optimist').argv;
+var url = require('url');
+
 var stream = nodecast.find();
+
+if (!argv._[0]) throw "Missing URL argument";
+var ytid = url.parse(argv._[0], true).query.v;
+if (!ytid) throw "Invalid URL";
 
 stream.on('device', function(device) {
 	// This is how you would do a device filter
@@ -11,7 +18,7 @@ stream.on('device', function(device) {
 
 	var youtube = device.app('YouTube');
 
-	youtube.start('v=oHg5SJYRHA0', function(err) {
+	youtube.start('v='+ytid, function(err) {
 		if (err) console.log('error starting', err);
 		console.log('Started on', device.name);
 	});
