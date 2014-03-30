1cf1d8e304697204b02b760352abc5e32da5e145
Contra <contra@maricopa.edu>
2014-03-30 16:32:34 -0700

    0.0.9

--- examples/list-devices.js
+++ examples/list-devices.js
@@ -4,5 +4,20 @@ var nodecast = require('../');
 var stream = nodecast.find();
 
 stream.on('device', function(device) {
-	console.log('Found', device.name, '-', device.info);
+	console.log('Found', device.name);//,  device.info);
+  if (typeof device.apps === 'function') {
+    device.apps(function(err, apps){
+      console.log('Applications for', device.name, apps);
+    });
+  }
+  if (typeof device.details === 'function') {
+    device.details(function(err, details){
+      console.log('Details for', device.name, details);
+    });
+  }
+  if (typeof device.running === 'function') {
+    device.running(function(err, app){
+      console.log('Currently running app for', device.name, app);
+    });
+  }
 });
